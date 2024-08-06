e6334710ed6f1e9c359019f5fe83906c0881479f
Hasan Hammad <hasanj2k@gmail.com>
2024-08-06 16:18:10 -0400

    Added authentication for the jwt token and added a temp route to test

---
+++ Auth/utils.py
@@ -0,0 +1,31 @@
+from functools import wraps
+from flask import request, jsonify
+import jwt
+from Models import User
+import os
+
+SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'Authorization' in request.headers:
+            auth_header = request.headers['Authorization']
+            token = auth_header.split(" ")[1]  # Extract token
+
+        if not token:
+            return jsonify({'message': 'Token is missing!', 'success': False}), 403
+
+        try:
+            data = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+            current_user = User.query.filter_by(id=data['sub']).first()
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Authorization Failed', 'success': False}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({'message': 'Authorization Failed', 'success': False}), 401
+        except Exception as e:
+            return jsonify({'message': f'An error occurred: {str(e)}', 'success': False}), 500
+
+        return f(current_user, *args, **kwargs)
+    return decorated
--- app.py
+++ app.py
@@ -6,12 +6,14 @@ from Models import db, User, Product, Order, OrderItem, Review
 from flask_cors import CORS
 from Blueprints.Routes.products import products_bp
 from Blueprints.Routes.register import register_bp
+from Blueprints.Routes.login import login_bp
+from Auth.utils import token_required
 
 
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 
 app.config['SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'default_secret_key')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
@@ -22,6 +24,13 @@ db.init_app(app)
 
 app.register_blueprint(products_bp)
 app.register_blueprint(register_bp)
+app.register_blueprint(login_bp)
+
+
+@app.route('/protected', methods=['GET'])
+@token_required
+def protected_route(current_user):
+    return jsonify({'message': f'Hello, {current_user.username}! This is a protected route. {current_user.email}', 'success': True})
 
 
 if __name__ == '__main__':
